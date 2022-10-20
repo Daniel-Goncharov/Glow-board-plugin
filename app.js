9d4e92008b1e2e0b7caacea1a83f8c67ccf83f71
Daniel Goncharov <goncharov10d@gmail.com>
2022-10-20 14:45:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board');
+const SQUARS_NUMBER = 700;
+const colors = ['#228653', '#BFB330', '#540EAD', '#A63A00', '#2C17B1', '#369100', '#F80012', '#015367'];
+
+for (let i = 0; i < SQUARS_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  board.append(square);
+
+  square.addEventListener('mouseleave', () => removeColor(square));
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
