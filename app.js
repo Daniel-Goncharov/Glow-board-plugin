dbc7a2ddd287b1ffdcb3d676437bb2603c6e76d5
Daniel Goncharov <goncharov10d@gmail.com>
2022-10-20 18:13:20 +0500

    refractoring

--- app.js
+++ app.js
@@ -6,24 +6,25 @@ for (let i = 0; i < SQUARS_NUMBER; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
 
-  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseover', setColor);
   board.append(square);
 
-  square.addEventListener('mouseleave', () => removeColor(square));
+  square.addEventListener('mouseleave', removeColor);
 }
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target;
   const color = getRandomColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target;
   element.style.backgroundColor = '#1d1d1d';
   element.style.boxShadow = '0 0 2px #000';
 }
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
